6ab9da7bc497508937f19991872048a2a8a7cb4b
macdonst <simon.macdonald@gmail.com>
2022-03-23 14:30:01 -0400

    Test auth
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app.arc
+++ app.arc
@@ -7,21 +7,30 @@ get /players
 post /players
 post /players/:id
 post /players/:id/delete
+get /games
+post /games
+post /games/:id
+post /games/:id/delete
 
 @plugins
 arc-plugin-oauth
 
 @oauth
 use-mock true
+allow-list allow.mjs
 
 @tables
 players
   email *String
 
+games
+  date *String
+
 @tables-indexes
 players
   fulltime *String
   name playersByFulltime
+
 
 @aws
 # profile default
--- src/http/get-index/index.mjs
+++ src/http/get-index/index.mjs
@@ -1,49 +1,38 @@
-export async function handler (req) {
-  console.log(req)
+import arc from '@architect/functions'
+import arcOauth from 'arc-plugin-oauth'
+const auth = arcOauth.auth
+const checkAuth = arcOauth.checkAuth
+
+export const handler = arc.http.async(auth, index)
+
+async function index (req) {
+  const authenticated = checkAuth(req)
+  console.log(authenticated)
+  console.log(req.session)
   return {
-    statusCode: 200,
-    headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
-    },
-    body: `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Architect</title>
-  <style>
-     * { margin: 0; padding: 0; box-sizing: border-box; } body { font-family: -apple-system, BlinkMacSystemFont, sans-serif; } .max-width-320 { max-width: 20rem; } .margin-left-8 { margin-left: 0.5rem; } .margin-bottom-16 { margin-bottom: 1rem; } .margin-bottom-8 { margin-bottom: 0.5rem; } .padding-32 { padding: 2rem; } .color-grey { color: #333; } .color-black-link:hover { color: black; }
-  </style>
-</head>
-<body class="padding-32">
-  <div class="max-width-320">
-    <img src="https://assets.arc.codes/logo.svg" />
-    <div class="margin-left-8">
-      <div class="margin-bottom-16">
-        <h1 class="margin-bottom-16">
-          Hello from an Architect Node.js function!
-        </h1>
-        <p class="margin-bottom-8">
-          Get started by editing this file at:
-        </p>
-        <code>
-          arc-fnh/src/http/get-index/index.js
-        </code>
-      </div>
-      <div>
-        <p class="margin-bottom-8">
-          View documentation at:
-        </p>
-        <code>
-          <a class="color-grey color-black-link" href="https://arc.codes">https://arc.codes</a>
-        </code>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
-`
+    html: `
+    <html>
+      <head>
+      </head>
+      <body>
+        <p> You are Authenticated </p>
+        <form method="post" action="/logout" >
+          <button type="submit">Logout</button>
+        </form>
+      </body>
+    </html>
+    `
   }
+
+  /*
+  if(authenticated) {
+    return {
+      html: ` <p> You are Authenticated <form method="post" action="/logout"><button type="submit">logout</button></form></p> `
+    }
+  } else{
+    return {
+      html: ` <p> You are a Guest <form method="get" action="/login"><button type="submit">login</button></form> </p> `
+    }
+  }
+  */
 }
