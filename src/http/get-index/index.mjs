3fa51d36c5c1ed9a6ec9bf3f08a262062e21f62b
macdonst <simon.macdonald@gmail.com>
2022-03-23 15:24:10 -0400

    Login test
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/http/get-index/index.mjs
+++ src/http/get-index/index.mjs
@@ -3,12 +3,13 @@ import arcOauth from 'arc-plugin-oauth'
 const auth = arcOauth.auth
 const checkAuth = arcOauth.checkAuth
 
-export const handler = arc.http.async(auth, index)
+export const handler = arc.http.async(index)
 
 async function index (req) {
   const authenticated = checkAuth(req)
   console.log(authenticated)
   console.log(req.session)
+    /*
   return {
     html: `
     <html>
@@ -23,16 +24,18 @@ async function index (req) {
     </html>
     `
   }
+  */
 
-  /*
+
   if(authenticated) {
     return {
-      html: ` <p> You are Authenticated <form method="post" action="/logout"><button type="submit">logout</button></form></p> `
+      html: ` <p> You are Authenticated <form method="post" action="/logout"><button type="submit">logout</button></form></p>
+      <p>${JSON.stringify(req.session)}</p> `
     }
   } else{
     return {
-      html: ` <p> You are a Guest <form method="get" action="/login"><button type="submit">login</button></form> </p> `
+      html: ` <p> You are a Guest <form method="get" action="/login"><button type="submit">login</button></form> </p>
+      <p>${JSON.stringify(req.session)}</p> `
     }
   }
-  */
 }
