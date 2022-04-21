72061b964c972eb0ea7fc121d960f5807ba446bc
macdonst <simon.macdonald@gmail.com>
2022-04-21 12:53:57 -0400

    Remove css vars
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/views/templates/enhance-table.mjs
+++ src/views/templates/enhance-table.mjs
@@ -1,18 +1,4 @@
-/*
-  Wrap the table in a div and override the following CSS values
-  to change the look and feel.
-
-  --table-bg-color, default: --grey-2
-  --th-bg-color, default: --grey0
-  --th-text-color, default: --grey6
-  --tbody-border-width, default: 2px
-  --tbody-border-color, default --grey3
-  --tbody-border-radius, default: 4px
- */
-
 export default function EnhanceTable({ html, state = {} }) {
-  // "--table-bg-color: blue; --th-bg-color: red; --th-text-color: white;">
-
   const { width = '100%' } = state.attrs
   return html`
     <style>
