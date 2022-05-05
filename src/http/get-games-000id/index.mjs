9f39409a09896eba94a2cc2356c682bd68c75b92
macdonst <simon.macdonald@gmail.com>
2022-05-05 00:27:27 -0400

    Tweak the game layout
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/http/get-games-000id/index.mjs
+++ src/http/get-games-000id/index.mjs
@@ -1,6 +1,10 @@
 import arc from '@architect/functions'
 import render from '@architect/views/render.mjs'
-import { getFulltimePlayers, getSpares } from '@architect/shared/db/players.mjs'
+import {
+  getFulltimePlayers,
+  getPlayerInfo,
+  getSpares
+} from '@architect/shared/db/players.mjs'
 import arcOauth from 'arc-plugin-oauth'
 import { getGame } from '@architect/shared/db/games.mjs'
 import { dateToEnglish } from '@architect/shared/utils.mjs'
@@ -38,6 +42,11 @@ async function gameStatus(req) {
   const spares = await getSpares()
   const spareSkaters = spares.filter((player) => player.position !== 'goalie')
   const spareGoalies = spares.filter((player) => player.position === 'goalie')
+  const cancellations = await getPlayerInfo(game.cancellations)
+
+  console.log(spares)
+
+  console.log(game.spares)
 
   return {
     html: render(
@@ -49,47 +58,84 @@ async function gameStatus(req) {
       <form form method="post" action="/games/${id}">
         <hockey-action-buttons direction="row-reverse">
           <hockey-button icon="save">Save</hockey-button>
-          <hockey-action-button action="/games/add?id=${id}" icon="write" label="Edit" type="link" variant="default" >Edit</hockey-action-button>
+          <hockey-action-button action="/games/add?id=${id}" icon="write" label="Edit" type="link" variant="default">Edit</hockey-action-button>
         </hockey-action-buttons>
-        <enhance-table>
-            <enhance-thead>
-              <enhance-tr><enhance-th>Name</enhance-th><enhance-th class="unseen">Position</enhance-th><enhance-th>Away</enhance-th><enhance-th>Spare</enhance-th></enhance-tr>
-            </enhance-thead>
-            <enhance-tbody>
-              ${[...skaters, ...goalies]
-                .map(function (player) {
-                  const cancelled = game.cancellations?.includes(player.email)
-                  const spare = cancelled ? game.spares?.shift() : ''
-                  return `<enhance-tr>
-                    <enhance-td class="${cancelled ? 'strikethrough' : ''}">
-                      ${player.name}
-                    </enhance-td>
-                    <enhance-td class="capitalize unseen">
-                      ${player.position}
-                    </enhance-td>
-                    <enhance-td>
-                      <input type="checkbox" name="cancellations" class="leading5-l pt-3 pb-3 pl-1 pr-1 radius2 shadow-2" value="${
+        <div class="grid row-auto col-2 gap-1">
+          <div>
+          <h2>Players</h2>
+            <enhance-table>
+                <enhance-thead>
+                  <enhance-tr><enhance-th>Name</enhance-th><enhance-th class="unseen">Pos</enhance-th><enhance-th>Away</enhance-th></enhance-tr>
+                </enhance-thead>
+                <enhance-tbody>
+                  ${[...skaters, ...goalies]
+                    .map(function (player) {
+                      const cancelled = game.cancellations?.includes(
                         player.email
-                      }" ${cancelled ? 'checked' : ''}/>
-                    </enhance-td>
+                      )
+                      return `<enhance-tr>
+                        <enhance-td class="${cancelled ? 'strikethrough' : ''}">
+                          ${player.name}
+                        </enhance-td>
+                        <enhance-td class="capitalize unseen">
+                          ${player.position[0]}
+                        </enhance-td>
+                        <enhance-td>
+                          <input type="checkbox" name="cancellations" class="leading5-l pt-3 pb-3 pl-1 pr-1 radius2 shadow-2" value="${
+                            player.email
+                          }" ${cancelled ? 'checked' : ''}/>
+                        </enhance-td>
+                      </enhance-tr>`
+                    })
+                    .join('')}
+                </enhance-tbody>
+              </enhance-table>
+            </div>
+            <div>
+              <h2>Spares</h2>
+              <enhance-table>
+                <enhance-thead>
+                  <enhance-tr><enhance-th>Name</enhance-th><enhance-th class="unseen">Pos</enhance-th></enhance-tr>
+                </enhance-thead>
+                <enhance-tbody>
+                  ${cancellations
+                    .map((cancellation) => {
+                      const spare = game.spares?.shift() || ''
+                      return `<enhance-tr>
                     <enhance-td>
                       ${
-                        cancelled && player.position !== 'goalie'
+                        cancellation.position !== 'goalie'
                           ? createSelectBox(spareSkaters, spare)
-                          : ''
-                      }
-                      ${
-                        cancelled && player.position === 'goalie'
-                          ? createSelectBox(spareGoalies, spare)
-                          : ''
+                          : createSelectBox(spareGoalies, spare)
                       }
                     </enhance-td>
+                    <enhance-td class="capitalize unseen">${
+                      cancellation.position[0]
+                    }</enhance-td>
                   </enhance-tr>`
-                })
-                .join('')}
-            </enhance-tbody>
-          </enhance-table>
-      <form>
+                    })
+                    .join('')}
+                </enhance-tbody>
+              </enhance-table>
+              <h2>Invitations</h2>
+              <enhance-table>
+                <enhance-thead>
+                  <enhance-tr><enhance-th>Name</enhance-th></enhance-tr>
+                </enhance-thead>
+                <enhance-tbody>
+                </enhance-tbody>
+              </enhance-table>
+              <h2>Declined</h2>
+              <enhance-table>
+                <enhance-thead>
+                  <enhance-tr><enhance-th>Name</enhance-th></enhance-tr>
+                </enhance-thead>
+                <enhance-tbody>
+                </enhance-tbody>
+              </enhance-table>
+            </div>
+            </div>
+        <form>
 
     </hockey-page>
 `,
