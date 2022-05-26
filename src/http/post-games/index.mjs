8627771f19f956af7a7da1c9d55988ef6a115d08
macdonst <simon.macdonald@gmail.com>
2022-05-26 11:27:20 -0400

    Issue #10: Editing a game causes a internal server error
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/http/post-games/index.mjs
+++ src/http/post-games/index.mjs
@@ -1,5 +1,6 @@
 import arc from '@architect/functions'
 import { upsertGame } from '@architect/shared/db/games.mjs'
+import { getSeasonWithGame } from '@architect/shared/db/seasons.mjs'
 import arcOauth from 'arc-plugin-oauth'
 const auth = arcOauth.auth
 
@@ -7,8 +8,9 @@ export const handler = arc.http.async(auth, http)
 
 async function http(req) {
   await upsertGame(req.body)
+  const season = await getSeasonWithGame(req.body.gamedate)
 
   return {
-    location: '/games'
+    location: `/games?season=${season.seasonID}`
   }
 }
